4332327b2a167f2e2f972e166fe610a48e075ad1
Lukas Niklas F <lukas@fruntke.tech>
2018-10-22 20:39:25 +0200

    :sparkles: Add content of the first lecture

---
+++ src/main/java/tech/fruntke/lectures/one/ArbitraryMessage.java
@@ -0,0 +1,7 @@
+package tech.fruntke.lectures.one;
+
+public class ArbitraryMessage {
+    public static void main(String[] args) {
+        System.out.println("eine beliebige Nachricht");
+    }
+}
---
+++ src/main/java/tech/fruntke/lectures/one/TenfoldNumber.java
@@ -0,0 +1,8 @@
+package tech.fruntke.lectures.one;
+
+public class TenfoldNumber {
+    public static void main(String[] args) {
+        int initialNumber = 5;
+        System.out.println("original Number: " + initialNumber + ". Multiplied with ten it gives: " + initialNumber * 10);
+    }
+}
