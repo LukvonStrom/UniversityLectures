516bd86e89f1871f870e46bacf8f77643fa08492
Lukas Niklas F <lukas@fruntke.tech>
2018-10-24 10:05:01 +0200

    :sparkles: Add content of the fifth/sixth lecture

---
+++ src/main/java/tech/fruntke/lectures/six/Radio.java
@@ -0,0 +1,71 @@
+package tech.fruntke.lectures.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Radio {
+    private boolean eingeschaltet;
+    // Lautstärke kann als absoluter Wert in db gesetzt sein (=> Datentyp = Integer)
+    // oder wie hier relativ als Prozentzahl zur Maximallautstärke
+    private double lautstaerke;
+    private double frequenz;
+    private List<Double> frequencyStorage = new ArrayList<Double>();
+
+    public Radio() {
+    }
+
+    public Radio(boolean eingeschaltet, double lautstaerke) {
+        this.eingeschaltet = eingeschaltet;
+        this.lautstaerke = lautstaerke;
+    }
+
+    public Radio(boolean eingeschaltet, double lautstaerke, double frequenz) {
+        this.eingeschaltet = eingeschaltet;
+        this.lautstaerke = lautstaerke;
+        this.frequenz = frequenz;
+        this.frequencyStorage.add(frequenz);
+    }
+
+
+    public void lauter() {
+        this.lautstaerke++;
+    }
+
+    public void lauter(int i) {
+        this.lautstaerke += i;
+    }
+
+    public void leiser() {
+        this.lautstaerke--;
+    }
+
+    public void leiser(int i) {
+        this.lautstaerke -= i;
+    }
+
+    public void an() {
+        this.eingeschaltet = true;
+    }
+
+    public void aus() {
+        this.eingeschaltet = false;
+    }
+
+    public boolean istAn() {
+        return this.eingeschaltet;
+    }
+
+    public double erhalteLautstaerke() {
+        return this.lautstaerke;
+    }
+
+    public void waehleSender(double frequenz) {
+        this.frequenz = frequenz;
+        this.frequencyStorage.add(frequenz);
+    }
+
+    @Override
+    public String toString() {
+        return "Radio mit Frequenz: " + this.frequenz + "ist An: " + this.eingeschaltet + " und Lautstärke: " + this.lautstaerke + " und Frequenzspeicher: " + frequencyStorage.toString();
+    }
+}
---
+++ src/main/java/tech/fruntke/lectures/six/Socke.java
@@ -0,0 +1,32 @@
+package tech.fruntke.lectures.six;
+
+public class Socke {
+    private String farbe;
+    private boolean istTrocken;
+
+    public Socke() {
+        this("schwarz");
+        this.istTrocken = false;
+    }
+
+    private Socke(String farbe) {
+        this.farbe = farbe;
+    }
+
+    public boolean istTrocken() {
+        return this.istTrocken;
+    }
+
+    public void trockne() {
+        this.istTrocken = true;
+    }
+
+    public void wasche() {
+        this.istTrocken = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Socke mit Farbe: " + this.farbe + " ist trocken " + (istTrocken ? "ja" : "nein");
+    }
+}
